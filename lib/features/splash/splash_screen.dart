b86a4c3e30558e1eb0bfc430ac50a5c7fad51293
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-10-19 18:54:21 +0300

    Update splash screen animation time

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -19,7 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(
-      const Duration(milliseconds: 1550),
+      const Duration(milliseconds: 1050),
       () => Navigator.pushReplacement(
         context,
         MaterialPageRoute(
@@ -41,7 +41,7 @@ class _SplashScreenState extends State<SplashScreen> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             TweenAnimationBuilder(
-              duration: const Duration(milliseconds: 1000),
+              duration: const Duration(milliseconds: 700),
               curve: Curves.bounceOut,
               tween: Tween(begin: 0.5, end: 0.08),
               builder: (context, value, child) => Transform.translate(
@@ -56,7 +56,7 @@ class _SplashScreenState extends State<SplashScreen> {
               ),
             ),
             TweenAnimationBuilder(
-              duration: const Duration(milliseconds: 1000),
+              duration: const Duration(milliseconds: 700),
               curve: Curves.bounceOut,
               tween: Tween(begin: 1, end: 0.11),
               builder: (context, value, child) => Transform.translate(
@@ -71,9 +71,9 @@ class _SplashScreenState extends State<SplashScreen> {
               ),
             ),
             TweenAnimationBuilder(
-              duration: const Duration(milliseconds: 1500),
+              duration: const Duration(milliseconds: 1000),
               curve: Curves.bounceOut,
-              tween: Tween(begin: 1.0, end: 0.035),
+              tween: Tween(begin: 1.0, end: 0.045),
               builder: (context, value, child) => Transform.translate(
                 offset: Offset(0, value * -500),
                 child: child,
